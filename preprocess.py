8d260b190e954f7ce0e627a6603630304088c97c
Nareman Dabbas <naremands@Naremans-MBP.lan1>
2020-05-10 15:24:04 -0500

    ADD the final results

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,12 @@
+
 def prep_data(df):
+    
+    df = df.assign(lhw=df["Height"]*df["Height"] * df["Width"])
 
-    df = df.assign(hw=df["Height"] * df["Width"])
-
-    X = df[["Height", "Width", "hw"]].values
-    y = df["Weight"].values
-
-    return X, y
+    X = df[["Length3","Height","Width","lhw"]].values
+    y = df["Weight"] .values
+    
+    
+    
+    
+    return X,y
--- split.py
+++ split.py
@@ -11,11 +11,14 @@ df_participant, df_holdout = train_test_split(df, random_state=42, test_size=.3,
 
 # I'll give you df_demo_holdout which is just s subset of df_participant
 # the point of this is so that you can test if your code works
-_, df_demo_holdout = train_test_split(df_participant)
+_, df_demo_holdout = train_test_split(df_participant, random_state=123,  stratify=df_participant["Species"])
 
 # You'll get:
 df_participant.to_csv("fish_participant.csv", index=False)
 df_demo_holdout.to_csv("fish_holdout_demo.csv", index=False)
+
+print(df_participant["Species"].value_counts())
+print(df_demo_holdout["Species"].value_counts())
 
 # We'll keep:
 df_holdout.to_csv("fish_holdout.csv", index=False)
